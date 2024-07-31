2d104fab1c0e242b47b14205d964a1570d34939a
J7 <j7.dev.gg@outlook.com>
2024-07-31 22:46:12 +0800

    chore: release v1.0.5

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Powerhouse
  * Plugin URI:        https://github.com/j7-dev/power-house
  * Description:       方便開發 WordPress 外掛的工具包。
- * Version:           1.0.4
+ * Version:           1.0.5
  * Requires at least: 5.7
  * Requires PHP:      8.0
  * Author:            J7
