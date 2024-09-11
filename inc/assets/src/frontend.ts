b2474cf356001847168010070e0f242e6b102f96
J7 <j7.dev.gg@gmail.com>
2024-09-11 11:31:08 +0800

    chore: 拆分 js

---
+++ inc/assets/src/frontend.ts
@@ -0,0 +1,9 @@
+import '@/assets/scss/index.scss'
+import $ from 'jquery'
+
+
+$(document).ready(() => {
+	// $('#powerhouse-settings').html(/*html*/`
+
+	// `)
+})
--- vite.config-for-wp.ts
+++ vite.config-for-wp.ts
@@ -7,41 +7,49 @@ import optimizer from 'vite-plugin-optimizer'
 import { terser } from 'rollup-plugin-terser'
 
 export default defineConfig({
-  build: {
-    emptyOutDir: true,
-    minify: true,
-    outDir: path.resolve(__dirname, 'inc/assets/dist'),
-    watch: {
-      include: 'inc/**',
-      exclude:
-        'js/**, modules/**, node_modules/**, release/**, vendor/**, .git/**, .vscode/**',
-    },
-    rollupOptions: {
-      input: 'inc/assets/src/main.ts', // Optional, defaults to 'src/main.js'.
-      output: {
-        assetFileNames: '[ext]/index.[ext]',
-        entryFileNames: 'index.js',
-      },
-    },
-  },
-  plugins: [
-    alias(),
-    tsconfigPaths(),
-    liveReload([
-      __dirname + '/**/*.php',
-    ]),
-    optimizer({
-      jquery: 'const $ = window.jQuery; export { $ as default }',
-    }),
-    terser({
-      mangle: {
-        reserved: ['$'], // 指定 $ 不被改變
-      },
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'inc/assets/src'),
-    },
-  },
+	build: {
+		emptyOutDir: true,
+		minify: true,
+		outDir: path.resolve(__dirname, 'inc/assets/dist'),
+		watch: {
+			include: '../*power*/inc/**',
+			exclude:
+				'js/**, modules/**, node_modules/**, release/**, vendor/**, .git/**, .vscode/**',
+		},
+		rollupOptions: {
+			input: {
+				admin: 'inc/assets/src/admin.ts',
+				frontend: 'inc/assets/src/frontend.ts',
+			},
+			output: {
+				entryFileNames: (assetInfo) => {
+					console.log(assetInfo)
+					return '[name].js'
+				},
+				assetFileNames: (assetInfo) => {
+					return '[ext]/index.[ext]'
+				},
+			},
+		},
+	},
+	plugins: [
+		alias(),
+		tsconfigPaths(),
+		liveReload([
+			__dirname + '/**/*.php',
+		]),
+		optimizer({
+			jquery: 'const $ = window.jQuery; export { $ as default }',
+		}),
+		terser({
+			mangle: {
+				reserved: ['$'], // 指定 $ 不被改變
+			},
+		}),
+	],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, 'inc/assets/src'),
+		},
+	},
 })
