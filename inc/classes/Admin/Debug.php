417f28c6a94465f6cc16eb33f4471d11fdd0f7f0
J7 <j7.dev.gg@gmail.com>
2024-12-17 19:29:23 +0800

    feat: debug log 新增前往底部

--- inc/classes/Admin/Debug.php
+++ inc/classes/Admin/Debug.php
@@ -32,28 +32,28 @@ final class Debug {
 	public function add_debug_submenu_page(): void {
 		global $submenu;
 
-		// 检查 tools.php 菜单是否存在
+		// 檢查 tools.php 選單是否存在
 		if (isset($submenu['tools.php'])) {
 			$debug_log_exists = false;
 
-			// 遍历 tools.php 的子菜单
+			// 遍歷 tools.php 的子選單
 			foreach ($submenu['tools.php'] as $item) {
-				// 检查是否已存在 debug-log-viewer
+				// 檢查是否已存在 debug-log-viewer
 				if ($item[2] === 'debug-log-viewer') {
 					$debug_log_exists = true;
 					break;
 				}
 			}
 
-			// 如果 debug-log-viewer 不存在，则添加子菜单
+			// 如果 debug-log-viewer 不存在，則添加子選單
 			if (!$debug_log_exists) {
 				\add_submenu_page(
-							'tools.php', // 父菜单文件，指向工具菜单
-							'Debug Log Viewer', // 页面标题
-							'Debug Log', // 菜单标题
-							'manage_options', // 所需的权限，例如管理选项
-							'debug-log-viewer', // 菜单slug
-							[ $this, 'debug_log_page_content' ], // 用于渲染页面内容的回调函数
+							'tools.php', // 父選單檔案，指向工具選單
+							'Debug Log Viewer', // 頁面標題
+							'Debug Log', // 選單標題
+							'manage_options', // 所需的權限，例如管理選項
+							'debug-log-viewer', // 選單slug
+							[ $this, 'debug_log_page_content' ], // 用於渲染頁面內容的回調函數
 							1000
 					);
 			}
@@ -66,7 +66,6 @@ final class Debug {
 	 * @return void
 	 */
 	public function debug_log_page_content(): void {
-		// 这里是渲染内容的函数，可以调用之前创建的 read_debug_log() 函数
 		printf(
 		/*html*/'
 		<div class="wrap">
@@ -74,12 +73,14 @@ final class Debug {
 			<p>只顯示 <code>/wp-content/debug.log</code> 最後 1000 行，
 				<a href="%s" target="_blank">下載 debug.log</a>
 			</p>
+			<p><a href="#bottom">前往底部</a></p>
 		</div>
 		',
 		\site_url( '/wp-content/debug.log' )
 		);
 
 		echo '<pre style="line-height: 0.75;">' . $this->read_debug_log() . '</pre>';
+		echo '<div id="bottom"></div>'; // 添加底部錨點
 	}
 
 
@@ -89,16 +90,16 @@ final class Debug {
 	 * @return string
 	 */
 	private function read_debug_log(): string {
-		$log_path = WP_CONTENT_DIR . '/debug.log'; // 使用 WP_CONTENT_DIR 常量定义日志文件路径
-		if ( \file_exists( $log_path ) ) { // 检查文件是否存在
-			$lines       = \file( $log_path ); // 读取文件到数组中，每行是一个数组元素
-			$last_lines  = \array_slice( $lines, -1000 ); // 获取最后1000行
-			$log_content = \implode( '', $last_lines ); // 将数组元素合并成字符串
+		$log_path = WP_CONTENT_DIR . '/debug.log'; // 使用 WP_CONTENT_DIR 常量定義日誌檔案路徑
+		if ( \file_exists( $log_path ) ) { // 檢查檔案是否存在
+			$lines       = \file( $log_path ); // 讀取檔案到陣列中，每行是一個陣列元素
+			$last_lines  = \array_slice( $lines, -1000 ); // 獲取最後1000行
+			$log_content = \implode( '', $last_lines ); // 將陣列元素合併成字串
 			if ( !$log_content ) {
-				// 处理读取错误
+				// 處理讀取錯誤
 				return 'Error reading log file.';
 			}
-			return \nl2br( \esc_html( $log_content ) ); // 将换行符转换为HTML换行，并转义内容以避免XSS攻击
+			return \nl2br( \esc_html( $log_content ) ); // 將換行符轉換為HTML換行，並轉義內容以避免XSS攻擊
 		}
 		return 'Log file does not exist.';
 	}
