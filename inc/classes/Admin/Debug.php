57a1df7dfd61c50363fe11ec3558657a9bb264ec
J7 <j7.dev.gg@gmail.com>
2025-01-10 12:20:16 +0800

    feat: 優化寫法

--- inc/classes/Admin/Debug.php
+++ inc/classes/Admin/Debug.php
@@ -7,6 +7,8 @@ declare(strict_types=1);
 
 namespace J7\Powerhouse\Admin;
 
+use J7\WpUtils\Classes\General;
+
 if ( class_exists( 'J7\Powerhouse\Admin\Debug' ) ) {
 	return;
 }
@@ -33,30 +35,24 @@ final class Debug {
 		global $submenu;
 
 		// 檢查 tools.php 選單是否存在
-		if (isset($submenu['tools.php'])) {
-			$debug_log_exists = false;
+		if (!isset($submenu['tools.php'])) {
+			return;
+		}
 
-			// 遍歷 tools.php 的子選單
-			foreach ($submenu['tools.php'] as $item) {
-				// 檢查是否已存在 debug-log-viewer
-				if ($item[2] === 'debug-log-viewer') {
-					$debug_log_exists = true;
-					break;
-				}
-			}
+		// 是否已添加過 debug_submenu
+		$debug_submenu = General::array_find($submenu['tools.php'], fn( $item ) => $item[2] === 'debug-log-viewer');
 
-			// 如果 debug-log-viewer 不存在，則添加子選單
-			if (!$debug_log_exists) {
-				\add_submenu_page(
-							'tools.php', // 父選單檔案，指向工具選單
-							'Debug Log Viewer', // 頁面標題
-							'Debug Log', // 選單標題
-							'manage_options', // 所需的權限，例如管理選項
-							'debug-log-viewer', // 選單slug
-							[ $this, 'debug_log_page_content' ], // 用於渲染頁面內容的回調函數
-							1000
-					);
-			}
+		// 沒找到 = 未添加過 debug_submenu
+		if (!$debug_submenu) {
+			\add_submenu_page(
+						'tools.php', // 父選單檔案，指向工具選單
+						'Debug Log Viewer', // 頁面標題
+						'Debug Log', // 選單標題
+						'manage_options', // 所需的權限，例如管理選項
+						'debug-log-viewer', // 選單slug
+						[ $this, 'debug_log_page_content' ], // 用於渲染頁面內容的回調函數
+						1000
+				);
 		}
 	}
 
@@ -93,6 +89,7 @@ final class Debug {
 		$log_path = WP_CONTENT_DIR . '/debug.log'; // 使用 WP_CONTENT_DIR 常量定義日誌檔案路徑
 		if ( \file_exists( $log_path ) ) { // 檢查檔案是否存在
 			$lines       = \file( $log_path ); // 讀取檔案到陣列中，每行是一個陣列元素
+			$lines       = is_array($lines) ? $lines : [];
 			$last_lines  = \array_slice( $lines, -1000 ); // 獲取最後1000行
 			$log_content = \implode( '', $last_lines ); // 將陣列元素合併成字串
 			if ( !$log_content ) {
