e46b437aca1fad99bd2d9fc10a08c6ae7ddd6b30
J7 <j7.dev.gg@gmail.com>
2025-03-27 19:42:16 +0800

    WIP: User 聯絡註記

--- inc/classes/Domains/User/Utils/CRUD.php
+++ inc/classes/Domains/User/Utils/CRUD.php
@@ -8,8 +8,8 @@ declare(strict_types=1);
 namespace J7\Powerhouse\Domains\User\Utils;
 
 use J7\WpUtils\Classes\WP;
-use Automattic\WooCommerce\Admin\API\Reports\Customers\Query as CustomersQuery;
-use J7\Powerhouse\Domains\Order\Utils\Info;
+use J7\WpUtils\Classes\WC;
+use J7\Powerhouse\Domains\Comment\Model\Comment;
 
 
 /**
@@ -64,143 +64,6 @@ abstract class CRUD {
 
 		/** @var array{ID?: int, user_pass?: string, user_login?: string, user_nicename?: string, user_url?: string, user_email?: string, display_name?: string, nickname?: string, ...}|object $data */
 		return \wp_update_user($data);
-	}
-
-
-	/**
-	 * Format user details
-	 *
-	 * @param int           $user_id  User ID.
-	 * @param array<string> $meta_keys  要暴露的前端 meta key
-	 * @return array{
-	 * id: string,
-	 *   user_login: string,
-	 *   user_email: string,
-	 *   display_name: string,
-	 *   user_registered: string,
-	 *   user_registered_human: string|null,
-	 *   user_avatar_url: mixed,
-	 *   description: string,
-	 *   roles: array<string>,
-	 *   billing_phone: string,
-	 *   pc_birthday: int|null,
-	 *   edit_url: string,
-	 *   date_last_active: string|null,
-	 *   date_last_order: string|null,
-	 *   orders_count: int|null,
-	 *   total_spend: string|null,
-	 *   avg_order_value: string|null
-	 * }|null
-	 */
-	public static function format_user_record( int $user_id, array $meta_keys = [] ): array|null {
-		$user = \get_user_by( 'ID', $user_id );
-
-		if ( ! $user ) {
-			return [
-				'id'                    => '',
-				'user_login'            => '',
-				'user_email'            => '',
-				'display_name'          => '訪客',
-				'user_registered'       => '',
-				'user_registered_human' => '',
-				'user_avatar_url'       => 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.vecteezy.com%2Fvector-art%2F26631837-incognito-icon-vector-symbol-design-illustration&psig=AOvVaw0KlMIe2lsttP8SC-47PjOn&ust=1741950083858000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPDiwPbzhowDFQAAAAAdAAAAABAb',
-				'description'           => '',
-				'roles'                 => [],
-				'billing_phone'         => '',
-				'edit_url'              => '',
-			];
-		}
-
-		$user_registered      = (string) $user->get( 'user_registered' );
-		$user_registered_time = \strtotime($user_registered);
-		$user_avatar_url      = \get_user_meta($user_id, 'user_avatar_url', true);
-		$user_avatar_url      = $user_avatar_url ? $user_avatar_url : \get_avatar_url( $user_id );
-
-		$meta_keys_array = self::get_meta_keys_array( $user, $meta_keys );
-
-		$base_array = [
-			'id'                    => (string) $user_id,
-			'user_login'            => $user->user_login,
-			'user_email'            => $user->user_email,
-			'display_name'          => $user->display_name,
-			'user_registered'       => $user_registered,
-			'user_registered_human' => $user_registered_time ? \human_time_diff( $user_registered_time ) : null,
-			'user_avatar_url'       => $user_avatar_url,
-			'description'           => $user->description,
-			'roles'                 => $user->roles,
-			'billing_phone'         => \get_user_meta($user_id, 'billing_phone', true),
-			'pc_birthday'           => ( (int) \get_user_meta($user_id, 'pc_birthday', true) ) ?: null,
-			'edit_url'              => \get_edit_user_link( $user_id ),
-		];
-
-		// 取得 customer 資料
-		$customers_query = new CustomersQuery(
-			[
-				'customers'    => [ $user_id ],
-				// If unset, these params have default values that affect the results.
-				'order_after'  => null,
-				'order_before' => null,
-			]
-			);
-
-		$customer_data    = $customers_query->get_data();
-		$customer_history = $customer_data->data[0] ?? null;
-
-		$base_array['date_last_active'] = $customer_history['date_last_active'] ?? null;
-		$base_array['date_last_order']  = $customer_history['date_last_order'] ?? null;
-		$base_array['orders_count']     = $customer_history['orders_count'] ?? null;
-		$base_array['total_spend']      = $customer_history['total_spend'] ?? null;
-		$base_array['avg_order_value']  = $customer_history['avg_order_value'] ?? null;
-
-		$formatted_array = array_merge(
-			$base_array,
-			$meta_keys_array
-		);
-
-		// ENHANCE 未來可能會有階層  上下線關係的 user!?
-
-		/** @var array{
-		 *   id: string,
-		 *   user_login: string,
-		 *   user_email: string,
-		 *   display_name: string,
-		 *   user_registered: string,
-		 *   user_registered_human: string|null,
-		 *   user_avatar_url: mixed,
-		 *   description: string,
-		 *   roles: array<string>,
-		 *   billing_phone: string,
-		 *   pc_birthday: int|null,
-		 *   edit_url: string,
-		 *   date_last_active: string|null,
-		 *   date_last_order: string|null,
-		 *   orders_count: int|null,
-		 *   total_spend: string|null,
-		 *   avg_order_value: string|null
-		 * } $formatted_array */
-		return $formatted_array;
-	}
-
-	/**
-	 * Format user details
-	 *
-	 * @param int           $user_id  User ID.
-	 * @param array<string> $meta_keys  要暴露的前端 meta key
-	 * @return array{id: string, user_login: string, user_email: string, display_name: string, user_registered: string, user_registered_human: string|null, user_avatar_url: mixed, description: string, ...}|null
-	 */
-	public static function format_user_details( int $user_id, array $meta_keys = [] ): array|null {
-		$new_meta_keys = [
-			...$meta_keys,
-			'first_name',
-			'last_name',
-			'description',
-		];
-
-		$user_record = self::format_user_record( $user_id, $new_meta_keys );
-
-		$extra_array = Info::to_user_array( $user_id );
-
-		return array_merge( $user_record, $extra_array );
 	}
 
 	/**
@@ -374,15 +237,13 @@ abstract class CRUD {
 		}
 
 		// 解析購物車資料
-		// TEST 印出 WC Logger 記得移除 ---- //
-		\J7\WpUtils\Classes\WC::log($session, '$session');
-		// ---------- END TEST ---------- //
 		$cart_items   = \maybe_unserialize($session['cart']);
 		$cart_content = [];
 
 		if (!empty($cart_items)) {
 			foreach ($cart_items as $cart_item_key => $values) {
-				$product_id = $values['product_id'];
+				// 如果 cart_item 是變體，則使用變體的 product_id
+				$product_id = $values['variation_id'] ?: $values['product_id'];
 				$product    = \wc_get_product($product_id);
 
 				if (!$product) {
@@ -390,17 +251,106 @@ abstract class CRUD {
 				}
 
 				$cart_content[] = [
-					'product_id'   => $product_id,
-					'product_name' => $product->get_name(),
-					'quantity'     => $values['quantity'],
-					'price'        => $product->get_price(),
-					'variation_id' => isset($values['variation_id']) ? $values['variation_id'] : 0,
-					'variation'    => isset($values['variation']) ? $values['variation'] : [],
-					'line_total'   => $values['line_subtotal'],
+					'product_id'    => (string) $product_id,
+					'product_name'  => $product->get_name(),
+					'quantity'      => $values['quantity'],
+					'price'         => (float) $product->get_price(),
+					'line_total'    => (float) $values['line_total'],
+					'product_image' => WC::get_image_url_by_product( $product ),
 				];
 			}
 		}
 
 		return $cart_content;
+	}
+
+	/**
+	 * 取得指定用戶 ID 的訂單資料
+	 *
+	 * @param int                  $user_id 用戶 ID
+	 * @param array<string, mixed> $args 查詢參數
+	 * @return array{
+	 *   order_id: int,
+	 *   order_date: string,
+	 *   order_date_human: string|null,
+	 *   order_total: string|float,
+	 *   order_status: string,
+	 * }[]
+	 */
+	public static function get_user_orders( int $user_id, array $args = [], $format = OBJECT ): array {
+		$default_args = [
+			'customer_id' => $user_id,
+			// 'status' => ['wc-processing', 'wc-on-hold'],
+			'limit'       => 5, // 預設拿5個訂單
+			'page'        => 1,
+			'orderby'     => 'date',
+			'order'       => 'DESC',
+		];
+
+		$args = \wp_parse_args( $args, $default_args );
+
+		$orders = \wc_get_orders( $args );
+
+		if (OBJECT === $format) {
+			return $orders;
+		}
+
+		$orders_data = [];
+		foreach ($orders as $order) {
+			$items = $order->get_items();
+
+			$order_items = [];
+			foreach ($items as $item) {
+				/** @var \WC_Order_Item_Product $item */
+				$product_id    = $item->get_variation_id() ?: $item->get_product_id();
+				$product       = \wc_get_product( $product_id );
+				$order_items[] = [
+					'product_id'    => (string) $product_id,
+					'product_name'  => $item->get_name(),
+					'quantity'      => $item->get_quantity(),
+					'price'         => (float) $product->get_price(),
+					'line_total'    => (float) $item->get_total(),
+					'product_image' => WC::get_image_url_by_product( $product ),
+				];
+			}
+
+			$orders_data[] = [
+				'order_id'     => (string) $order->get_id(),
+				'order_date'   => $order->get_date_created()?->date('Y-m-d H:i:s'),
+				'order_total'  => (float) $order->get_total(),
+				'order_status' => $order->get_status(),
+				'order_items'  => $order_items,
+			];
+		}
+
+		return $orders_data;
+	}
+
+	/**
+	 * 取得指定用戶 ID 的聯絡註記
+	 *
+	 * @param int $user_id 用戶 ID
+	 * @return array<string, mixed>
+	 */
+	public static function get_contact_remarks( int $user_id ): array {
+
+		$user = \get_user_by( 'id', $user_id );
+		if (!$user) {
+			return [];
+		}
+
+		$args = [
+			'type'       => 'review', // 'contact_remarks',
+			'author__in' => [ $user_id ],
+			'status'     => 'approve', // 'hold' (`comment_status=0`), 'approve' (`comment_status=1`), 'all', or a custom comment status
+			'fields'     => 'ids',
+		];
+
+		$comment_ids = \get_comments( $args );
+		$comments    = [];
+		foreach ($comment_ids as $comment_id) {
+			$comments[] = Comment::instance( $comment_id )->to_array();
+		}
+		return $comments;
 	}
 }
