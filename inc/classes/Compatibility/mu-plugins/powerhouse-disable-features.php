4c163ca7c22ee10981082b2a55a87a0646b248c4
J7 <j7.dev.gg@gmail.com>
2025-08-07 13:02:45 +0800

    feat: 新增 DisableFeatures 類別以禁用 XML-RPC 和 REST API 使用者端點

---
+++ inc/classes/Compatibility/mu-plugins/powerhouse-disable-features.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * Plugin Name:       Disable Features | Powerhouse
+ * Plugin URI:        https://www.powerhouse.cloud
+ * Description:       安全性考量，禁用一些功能
+ * Version:           1.0.0
+ * Requires at least: 5.7
+ * Requires PHP:      8.0
+ * Author:            J7
+ * Author URI:        https://github.com/j7-dev
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       powerhouse
+ * Domain Path:       /languages
+ * Tags: vite, WordPress plugin
+ *
+ * *******************************************************************************************
+ *                                                                                           *
+ *   ██████╗  ██████╗ ██╗    ██╗███████╗██████╗ ██╗  ██╗ ██████╗ ██╗   ██╗███████╗███████╗   *
+ *   ██╔══██╗██╔═══██╗██║    ██║██╔════╝██╔══██╗██║  ██║██╔═══██╗██║   ██║██╔════╝██╔════╝   *
+ *   ██████╔╝██║   ██║██║ █╗ ██║█████╗  ██████╔╝███████║██║   ██║██║   ██║███████╗█████╗     *
+ *   ██╔═══╝ ██║   ██║██║███╗██║██╔══╝  ██╔══██╗██╔══██║██║   ██║██║   ██║╚════██║██╔══╝     *
+ *   ██║     ╚██████╔╝╚███╔███╔╝███████╗██║  ██║██║  ██║╚██████╔╝╚██████╔╝███████║███████╗   *
+ *   ╚═╝      ╚═════╝  ╚══╝╚══╝ ╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝  ╚═════╝ ╚══════╝╚══════╝   *
+ *                                                                                           *
+ * *********************************** www.powerhouse.cloud **********************************
+ */
+
+namespace J7\Powerhouse\MU;
+
+/**
+ * DisableFeatures
+ * 禁用 xmlrpc、rest api users 功能
+ */
+final class DisableFeatures
+{
+
+	const PRIORITY = 999;
+
+	/** Constructor */
+	public function __construct()
+	{
+		// 停用 XML-RPC
+		\add_filter('xmlrpc_enabled', '__return_false', self::PRIORITY);
+
+
+		// 停用 REST API 的使用者端點
+		\add_filter('rest_endpoints', function ($endpoints) {
+			// 移除所有使用者清單的 GET 請求
+			if (isset($endpoints['/wp/v2/users'])) {
+				unset($endpoints['/wp/v2/users']);
+			}
+			// 移除單一使用者資訊的 GET 請求
+			if (isset($endpoints['/wp/v2/users/(?P<id>[\d]+)'])) {
+				unset($endpoints['/wp/v2/users/(?P<id>[\d]+)']);
+			}
+			return $endpoints;
+		}, self::PRIORITY);
+	}
+}
+
+new DisableFeatures();
